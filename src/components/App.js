fadf68c0881176231b38960530f7965579a4bfa1
braxtonshaffie <braxtonshaffie@gmail.com>
2023-04-21 21:53:24 +0300

    done with 2 test

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
+
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
@@ -12,8 +13,9 @@ function App() {
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <NewTaskForm  />
+      <TaskList  tasks={TASKS} category={CATEGORIES}/>
+    
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+
+function TaskList({tasks,category}) {
+  console.log(tasks, category)
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+   <Task 
+        text={tasks.text}
+        category={category.category}
+   />
+
     </div>
   );
 }
